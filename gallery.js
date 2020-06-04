f753d8ef959997e922d8015455aef485ba1037d1
bigern83 <bigern2009@gmail.com>
2020-06-04 22:24:28 +0100

    image gallery

--- gallery.js
+++ gallery.js
@@ -1,28 +1,69 @@
-var gallery = document.querySelector("#mob-gallery")
-var gallMain = document.querySelector("#mob-gallery .gallery__main-image")
-var gallThumbs = document.querySelectorAll("#mob-gallery .gallery__thumbnail-item")
 
-gallMain.src = gallThumbs[0].firstChild.firstChild.src;
+var resizeGallery = function (el) {
+    var gallWidth = el.getBoundingClientRect().width;
+    
+    if (el.classList.contains("gallery--narrow")) {
+        el.style.height = String(1.1 * gallWidth) + "px";
+    } else {
+        el.style.height = String(0.8 * gallWidth) + "px";
+    }
+    console.log(el, gallWidth)
+}
+
+var resizeAll = function () {
+    var all = document.querySelectorAll(".gallery");
+    for (var i = 0; i < all.length; ++i) {
+        resizeGallery(all[i]);
+    }
+}
+
+var configureGallery = function (el) {
+
+    var thumbs = el.querySelectorAll(".gallery__thumbnails .gallery__thumbnail-item");
+    var main = el.querySelector(".gallery__main")
 
 
-var resizeGallery = function() {
-    var gallWidth = gallery.getBoundingClientRect().width;
-    gallery.style.height = String(0.8 * gallWidth) + "px";
-    console.log(gallWidth)
+    for (var i = 0; i < thumbs.length; ++i) {
+        console.log(i + "heljaljdflajdlfkj");
+        thumbs[i].addEventListener("click", function (ev) {
+           
+            for (var j = 0; j < main.children.length; ++j) {
+                if (this.firstChild.src === main.children[j].src) {
+                    console.log("matched");
+                    main.children[j].classList.remove("hidden");
+                    main.children[j].classList.add("visible");
+
+                } else {
+                    main.children[j].classList.remove("visible");
+                    main.children[j].classList.add("hidden");
+                }
+            }
+        });
+
+        var newImg = document.createElement("img");
+        newImg.src = thumbs[i].firstChild.src;
+        newImg.classList.add("gallery__image")
+        if (i === 0) {
+            newImg.classList.add("visible");
+
+        } else {
+
+            newImg.classList.add("hidden")
+        }
+        main.appendChild(newImg);
+    }
 }
 
 
+//get all instances of gallery
+    var galleryList = document.querySelectorAll(".gallery");
 
-for (var i = 0; i < gallThumbs.length; ++i) {
-    console.log(i);
-    gallThumbs[i].addEventListener("click", function (ev) {
-        console.log(this.children[0].innerHTML);
-        console.log(this.firstChild.firstChild.src)
-        gallMain.src = this.firstChild.firstChild.src;
+    for (var i = 0; i < galleryList.length; ++i) {
+        resizeAll();
+        configureGallery(galleryList[i])
+    }
 
-    });
-}
 
-console.log(gallery);
-resizeGallery();
-window.onresize = resizeGallery;
+
+resizeAll();
+window.onresize = resizeAll;
