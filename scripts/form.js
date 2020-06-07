4075bb88e5c57ba9b0551d32601705dcee3232b8
bigern83 <bigern2009@gmail.com>
2020-06-07 20:13:22 +0100

    validation on form

---
+++ scripts/form.js
@@ -0,0 +1,33 @@
+var defaultConfig = {
+    // class of the parent element where the error/success class is added
+    classTo: 'booking__group',
+    errorClass: 'form-error',
+    successClass: 'form-success',
+    // class of the parent element where error text element is appended
+    errorTextParent: 'booking__group',
+    // type of element to create for the error text
+    errorTextTag: 'span',
+    // class of the error text element
+    errorTextClass: 'text-help'
+};
+
+window.onload = function () {
+
+    var form = document.getElementById("booking-form");
+
+    // create the pristine instance
+    var pristine = new Pristine(form, defaultConfig);
+
+    form.addEventListener('submit', function (e) {
+ 
+
+        // check if the form is valid
+        console.log("submit")
+        var valid = pristine.validate(); // returns true or false
+        console.log(valid)
+        if (!valid) {
+            e.preventDefault();
+        }
+
+    });
+};
